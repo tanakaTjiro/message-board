157abe2ae677871a3600e07cc830ab3f25c2f0b3
tanakajiro <tanaka.j.tomohiro@gmail.com>
2016-11-22 05:26:05 +0000

    challenge2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -40,12 +40,11 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
   ## ここまで
 
   def set_message
     @message = Message.find(params[:id])
   end
-
 end
